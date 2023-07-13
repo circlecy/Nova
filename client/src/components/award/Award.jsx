6472c3982bf2ac51a4e1dd635e64740e6a7f9d1b
unknown <manileehk1@gmail.com>
2023-07-13 21:54:07 +0800

    30 commit

--- client/src/components/award/Award.jsx
+++ client/src/components/award/Award.jsx
@@ -7,7 +7,7 @@ export default function Award() {
         <div className="h-award">
             <div className="container">
                 <div className="row">
-                    <div className="col-xs-6 col-md-6" style={{minwidth: "720px"}}>
+                    <div className="col-xs-6 col-md-6" style={{ minwidth: "720px" }}>
                         <div className="photoGroup">
                             <img className="rect" alt="" src={require('../../assets/img/award_rect.svg').default} />
                             <img className="dot" alt="" src={require('../../assets/img/award_icon.svg').default} />
@@ -35,13 +35,13 @@ export default function Award() {
                             <h2>銳意拓展高淨值與大灣區客群</h2>
                             <div className="text">
                                 隨着團隊規模不斷擴展，除了強積金市場外，粵港澳大灣區訪港客戶市場，以及高淨值客戶群乃團隊未來的發展重點之一。「Sun Life永明是一間以人為本，且在強積金市場擁有豐富經驗的公司，近年更銳意發展大灣區市場。配合有關發展，團隊會持續增員，並進一步協助成員提升專業水平，以及Nova的整體服務質素。」
-                                <br/><br/>
+                                <br /><br />
                                 Nova致力為成員營造開心、愉快的工作環境，但同樣重視他們的專業水平，以及對工作的投入度。而團隊目前約有50%成員取得百萬圓桌會（MDRT）會員資格。
                             </div>
                             <br />
                             <div className="stat row">
                                 <div className="first col-xs-4 col-md-4">
-                                    <h3>15+</h3>
+                                    <h3>60+</h3>
                                     <span className="text">團隊成員</span>
                                 </div>
                                 <div className="second col-xs-4 col-md-4">
@@ -49,13 +49,13 @@ export default function Award() {
                                     <span className="text">團隊獎項</span>
                                 </div>
                                 <div className="third col-xs-4 col-md-4">
-                                    <h3>50%</h3>
+                                    <h3>60%</h3>
                                     <span className="text">團隊成員取得MDRT會員資格</span>
                                 </div>
                             </div>
                             <br />
                             <div className="button">
-                                <Link to="/about"><button className="button-17">查看更多</button></Link>
+                                <Link to="/award"><button className="button-17">查看更多</button></Link>
                             </div>
                         </div>
                     </div>
