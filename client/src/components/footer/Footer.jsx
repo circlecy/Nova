d1ce79c2fba33f1cadef00a6aa4f1a0bfe9a8b72
unknown <manileehk1@gmail.com>
2023-07-25 20:42:47 +0800

    37 commit

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -13,28 +13,29 @@ export default function Footer() {
                     <div className="col-sm-12 col-md-6">
                         <div><img src={logoimg} alt="" className="logoImg" /></div>
                         <ul className="footer-links">
-                            <li>Email : support@email.com </li>
-                            <li>電話 : 2567-8930</li>
-                            <li>地址: 455, Hong Kong</li>
+                            <li>Email : nova.recuitmentpower@gmail.com </li>
+                            <li>地址: 香港九龍紅磡紅鸞道18號祥祺中心A座7樓</li>
+                        </ul>
+                    </div>
+
+                    <div className="col-xs-6 col-md-3">
+                        <h6>關於及查詢</h6>
+                        <ul className="footer-links">
+                            <li><Link className="link" to="/about">關於我們 </Link></li>
+                            <li><Link className="link" to="/award">團隊成就</Link></li>
+                            <li><a href="mailto:nova.recuitmentpower@gmail.com?subject=Request to join Nova Team&body=I would like to join the Nova Team." className="link">加入我們</a></li>
+                            <li><Link className="link" to="/contact">聯絡我們</Link></li>
+
                         </ul>
                     </div>
 
                     <div className="col-xs-6 col-md-3">
                         <h6>網站地址</h6>
                         <ul className="footer-links">
-                            <li><Link className="link" to="/about">關於我們 </Link></li>
-                            <li><Link className="link" to="/posts">活動</Link></li>
-                            <li><Link className="link" to="/posts">加入我們</Link></li>
-                            <li><Link className="link" to="/about">聯絡及查詢</Link></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-xs-6 col-md-3">
-                        <h6></h6>
-                        <ul className="footer-links">
+                            <li><Link className="link" to="/posts">最新動態</Link></li>
+                            <li><Link className="link" to="/posts">活動相簿</Link></li>
+                            <li><Link className="link" to="/team">團隊感想</Link></li>
                             <li><Link className="link" to="/login">登入</Link></li>
-                            <li><Link className="link" to="/posts">活動</Link></li>
-                            <li><Link className="link" to="/contact">聯絡我們</Link></li>
                         </ul>
                     </div>
                 </div>
@@ -49,10 +50,8 @@ export default function Footer() {
 
                     <div className="col-md-4 col-sm-6 col-xs-12">
                         <ul className="social-icons">
-                            <li><div className="facebook icon"><a href=""><i className="fa-brands fa-facebook-f"></i></a></div></li>
-                            <li><div className="twitter icon"><a href=""><i className="fa-brands fa-twitter"></i></a></div></li>
-                            <li><div className="linkedin icon"><a href="#"><i className="fa-brands fa-linkedin-in"></i></a></div></li>
-                            <li><div className="instagram icon"><a href="#"><i className="fa-brands fa-instagram"></i></a></div></li>
+                            <li><div className="facebook icon"><a href="www.facebook.com"><i className="fa-brands fa-facebook-f"></i></a></div></li>
+                            <li><div className="instagram icon"><a href="www.instagram.com"><i className="fa-brands fa-instagram"></i></a></div></li>
                         </ul>
                     </div>
                 </div>
--- client/src/pages/album/Album.jsx
+++ client/src/pages/album/Album.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { Context } from "../../context/Context";
-import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import "./album.css";
 
@@ -11,26 +10,29 @@ export default function Album() {
     const { user } = useContext(Context);
     const [album, setAlbum] = useState([]);
     const [folder, setFolder] = useState("");
-    const { search } = useLocation();
+    const [current, setCurrent] = useState(0);
+    const videos = ["nova 2022.mp4", "nova 2021.mp4"];
+    const slides = document.getElementsByClassName("slide-image");
+    const bubbles = document.getElementsByClassName("bubble-outer");
     const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_URL, });
 
     useEffect(() => {
+        bubbles[current].style.borderColor = "white";
         const fetchAlbum = async () => {
-            const res = await axiosInstance.get("/albums" + search);
+            const res = await axios.get("/albums");
             setAlbum(res.data);
         };
         fetchAlbum();
-    }, [search]);
+    }, [bubbles, current]);
 
+    //dialog 
     const modal = document.querySelector('#my-modal');
     const openModal = () => { modal.style.display = 'block'; };
     const closeModal = () => { modal.style.display = 'none'; };
 
     window.addEventListener('click', outsideClick);
     function outsideClick(e) {
-        if (e.target == modal) {
-            modal.style.display = 'none';
-        }
+        if (e.target === modal) { modal.style.display = 'none'; }
     }
 
     const handleCreate = async (e) => {
@@ -46,18 +48,85 @@ export default function Album() {
         } catch (err) { }
     };
 
+    ///////////////////////////////////////////////////////////////
+
+    const nextImage = () => {
+        let newIndex = (current === videos.length - 1 ? 0 : current + 1);
+        slides[current].className = "slide-image leftOut";
+        slides[newIndex].className = "slide-image leftIn";
+        updateBubbles(newIndex);
+        setCurrent(newIndex);
+    }
+    const prevImage = () => {
+        let newIndex = (current === 0 ? videos.length - 1 : current - 1);
+        slides[current].className = "slide-image rightOut";
+        slides[newIndex].className = "slide-image rightIn";
+        updateBubbles(newIndex);
+        setCurrent(newIndex);
+    }
+    const jumpImage = (e) => {
+        let jumpIndex = parseInt(e.target.id);
+        if (jumpIndex === current) return;
+        if (jumpIndex - current >= 0) {
+            slides[current].className = "slide-image leftOut";
+            slides[jumpIndex].className = "slide-image leftIn";
+        } else {
+            slides[current].className = "slide-image rightOut";
+            slides[jumpIndex].className = "slide-image rightIn";
+        }
+        updateBubbles(jumpIndex);
+        setCurrent(jumpIndex);
+    }
+    const updateBubbles = (highlight) => {
+        bubbles[current].style.borderColor = "black";
+        bubbles[highlight].style.borderColor = "white";
+    }
 
     return (
         <div className="album">
+            <div className="gallery-container">
+                <span className="button-prev" onClick={prevImage}>chevron_left</span>
+                <span className="button-next" onClick={nextImage}>chevron_right</span>
+                <div className="gallery-track">
+                    {
+                        videos.map((video, index) => {
+                            return (
+                                <div className="slide-image" style={(index === 0) ?
+                                    { transform: "translateX(0%)" } : { transform: "translateX(100%)" }}>
+                                    <div className="row">
+                                        <div className="col-sm-6">
+                                            <h1 className="album-title__title">{(index === 0 ? "2022 Nova Team" : "2021 Nova Team")}</h1>
+                                            <h1 className="album-title__subtitle">活動片段</h1>
+                                        </div>
+                                        <div className="col-sm-6">
+                                            <video width="100%" height="350" src={PF + "/" + video} type="video/mp4" controls />
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="gallery-footer">
+                    {
+                        videos.map((image, index) => {
+                            return <div className="bubble-outer" onClick={jumpImage} id={index}>
+                                <div className="bubble-inner" id={index}></div>
+                            </div>
+                        })
+                    }
+                </div>
+            </div>
+
             <main className="wrapper">
-                <section className="album-title bg-dark-overlay text-center">
+                {/*<section className="album-title text-center">
                     <div className="container">
                         <div className="album-title__holder">
                             <h1 className="album-title__title">活動相簿</h1>
                             <p className="album-title__subtitle"></p>
                         </div>
                     </div>
-                </section>
+                </section>*/}
                 <div className="dialog">
                     {user ? (
                         <button id="modal-btn" className="button" onClick={openModal}>新增相簿</button>
@@ -87,13 +156,13 @@ export default function Album() {
                     <ul>
                         {album.slice().reverse().map((album, i) => (
                             <li key={i}>
-                                {album.album && <img className="postImg" src={!album.photos.length ? require('../../assets/img/Nova-Banner.jpg') : PF + album.album + "/" + album.photos[0].name} alt="" />}
-                                <div className="postInfo link">
+                                {album.album && <img className="albumImg" src={!album.photos.length ? require('../../assets/img/Nova-Banner.jpg') : PF + album.album + "/" + album.photos[0].name} alt="" />}
+                                <div className="albumInfo link">
                                     <Link to={`/album/${album.album}`} className="link">
                                         <span className="albumTitle">{album.album}</span>
                                     </Link>
                                 </div>
-                                {/*<p className="postDesc">123</p>*/}
+                                {/*<p className="postDesc"></p>*/}
                             </li>
                         ))}
                     </ul>
